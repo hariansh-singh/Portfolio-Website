4a760c3110b6ab4e82527181a377cfb11e4ebc09
hariansh-singh <harianshsingh13@gmail.com>
2024-04-08 14:25:10 +0530

    Updated reveal icons animation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,6 +195,8 @@ srtop.reveal('.home .content p', { delay: 200 });
 srtop.reveal('.home .content .btn', { delay: 200 });
 
 srtop.reveal('.home .image', { delay: 400 });
+srtop.reveal('.home .kaggle', { interval: 600 });
+srtop.reveal('.home .hashnode', { interval: 600 });
 srtop.reveal('.home .linkedin', { interval: 600 });
 srtop.reveal('.home .github', { interval: 800 });
 srtop.reveal('.home .twitter', { interval: 1000 });
